f8f391cf8daac3d3738bddc82662210a81848cc7
1846689910 <eric14706@gmail.com>
2020-07-05 08:50:36 -0700

    clear

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -1,28 +1,9 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import {
-  Grid,
-  Container,
-  Paper,
-  makeStyles,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Typography,
-} from "@material-ui/core";
+import React from "react";
+import { Typography } from "@material-ui/core";
 import ControlledAccordion, {
   AccordionData,
 } from "../client/js/components/Skills/ControlledAccordion";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Skill from "../client/js/components/Skills/Skill";
-
-const useStyles = makeStyles((theme) => ({
-  grid: {
-    height: "70vh",
-    overflow: "scroll",
-  },
-  accordion: {},
-}));
 
 const webSkills = [
   { name: "JavaScript", value: 85, src: "/skills-icon/js.png" },
@@ -35,7 +16,6 @@ const dataSkills = [
 ];
 
 export default function Skills() {
-  const classes = useStyles();
   const dataArray = [
     new AccordionData(
       <Typography>Web Development</Typography>,
@@ -48,7 +28,5 @@ export default function Skills() {
       "data",
     ),
   ];
-  return (
-    <ControlledAccordion dataArray={dataArray}/>
-  );
+  return <ControlledAccordion dataArray={dataArray} />;
 }
