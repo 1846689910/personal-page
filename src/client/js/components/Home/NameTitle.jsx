17837468074c5725d2833733ccef8c2f73736b3d
1846689910 <eric14706@gmail.com>
2020-07-05 23:17:38 -0700

    mod

--- src/client/js/components/Home/NameTitle.jsx
+++ src/client/js/components/Home/NameTitle.jsx
@@ -1,5 +1,6 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import { Grid, makeStyles, Typography } from "@material-ui/core";
+import MediaQueryContext from "../MediaQueryContext";
 
 const useStyles = makeStyles({
   name: {
@@ -9,15 +10,22 @@ const useStyles = makeStyles({
 
 export default function NameTitle() {
   const classes = useStyles();
+  const { isTabletOrMobile } = useContext(MediaQueryContext);
   return (
     <Fragment>
       <Grid item container justify="center">
-        <Typography className={classes.name} variant="h3">
+        <Typography
+          className={classes.name}
+          variant={isTabletOrMobile ? "h5" : "h3"}
+        >
           Zuhui(Eric) He
         </Typography>
       </Grid>
       <Grid item container justify="center">
-        <Typography className={classes.name} variant="h4">
+        <Typography
+          className={classes.name}
+          variant={isTabletOrMobile ? "body1" : "h4"}
+        >
           Software Engineer @ Apple Inc.
         </Typography>
       </Grid>
